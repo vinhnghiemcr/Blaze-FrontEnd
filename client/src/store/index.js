d05740aa42dd11222707e10c32931fc2e962d941
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-16 11:34:20 -0600

    updates

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,16 +1,15 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import UserReducer from './reducers/UserReducer'
 import StateReducer from './reducers/StateReducer'
-
 
 const store = createStore(
   combineReducers({
     userState: UserReducer,
     locationState: StateReducer
   }),
-  composeWithDevTools(applyMidleware(thunk))
+  composeWithDevTools(applyMiddleware(thunk))
 )
 
 export default store
