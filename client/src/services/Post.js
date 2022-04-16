9a3e337a991d4cd308667e7942a8ed045a1634a0
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-16 10:17:58 -0600

    Post services update

---
+++ client/src/services/Post.js
@@ -0,0 +1,46 @@
+import Client from './api'
+
+export const GetPostofTrail = async (trailId) => {
+  try {
+    const res = await Client.get(`/trail/${trailId}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const GetPostofUser = async (userId) => {
+  try {
+    const res = await Client.get(`/trail/user/${userId}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CreatePost = async (userId, trailId) => {
+  try {
+    const res = await Client.post(`/post/trail/${trailId}/user/${userId}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const DeletePost = async (userId, postId) => {
+  try {
+    const res = await Client.delete(`/post/${postId}/user/${userId}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const UpdatePost = async (userId, postId) => {
+  try {
+    const res = await Client.put(`/post/${postId}/user/${userId}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
