a0833e94cbfb928e16cae8ed342918a71d534885
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-04-16 02:40:54 -0500

    Create user state in store

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './styles/index.css'
 import App from './App'
+import { Provider } from 'react-redux'
+import store from './store'
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>
 )
---
+++ client/src/store/reducers/UserReducer.js
@@ -0,0 +1,21 @@
+const { SET_USER, TOGGLE_AUTHENTICATED } = require('../types')
+
+const iState = {
+  user: null,
+  authenticated: false
+}
+
+const UserReducer = (state = iState, action) => {
+  switch (action.type) {
+    case SET_USER:
+      return { ...state, user: action.payload }
+    case SET_USER_NULL:
+        return { ...state, user: action.payload }
+    case TOGGLE_AUTHENTICATED:
+      return { ...state, authenticated: action.payload }
+    default:
+      return { ...state }
+  }
+}
+
+export default UserReducer
