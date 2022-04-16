6861794e7d2e9f096844bf3125d0faca3229761e
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-16 12:01:51 -0600

    State + Grid update"

--- client/src/components/State.jsx
+++ client/src/components/State.jsx
@@ -1,10 +1,12 @@
 
 
-const State = () => {
+const State = ({state}) => {
 
     return (
-        <div>
-            Hello
+        <div className="stateMap">
+            <h2>{state.name}</h2>
+            <img className="stateImg" src={state.img} alt={state.name}/>
+            <h4>{state.description}</h4>
         </div>
     )
 }
