9629b860404878622d377bd2bf69af0450a45d07
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-04-19 10:58:04 -0500

    Implemented OpenLayer API

---
+++ client/src/components/MapWrapper.jsx
@@ -0,0 +1,65 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import View from 'ol/View';
+import XYZ from 'ol/source/XYZ';
+import {fromLonLat} from 'ol/proj';
+import {Point} from 'ol/geom';
+import {Feature, Overlay} from 'ol/index';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import {Circle, Fill, Style} from 'ol/style';
+
+const KEY = process.env.REACT_APP_MAPTILER_KEY
+const attributions =
+  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
+  
+const MapWrapper = ({trailId}) => {
+  
+  const point = new Point(fromLonLat([-107.504491, 37.912982]));
+  const initmap = () => {
+    const map = new Map({
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            attributions: attributions,
+            url:
+              'https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=' + KEY,
+        }),
+      }),
+        new VectorLayer({
+          source: new VectorSource({
+            features: [new Feature(point)],
+          }),
+          style: new Style({
+            image: new Circle({
+              radius: 9,
+              fill: new Fill({color: 'red'}),
+            }),
+          }),
+        }),      
+      ],
+      target: trailId,
+      view: new View({
+        center: fromLonLat([-107.504491, 37.912982]),
+        zoom: 11,
+        minZoom: 9,
+        maxZoom: 13,
+      }),
+    });
+  }
+
+
+  window.onload = initmap
+
+  return (
+    <div>
+      <div id={trailId} className="map"></div>
+    </div>
+  )
+}
+
+export default MapWrapper
+
+ 
--- client/src/pages/TrailPage.js
+++ client/src/pages/TrailPage.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { GetPostByTrailId } from '../store/actions/PostActions'
 import Post from '../components/Post'
+import MapWapper from '../components/MapWrapper'
+
+
+
 
 const mapStateToProps = ({ postState }) => {
   return { postState }
@@ -21,12 +25,17 @@ const TrailPage = (props) => {
     props.fetchPosts(trailId)
   }, [])
 
-  console.log('props.postState.posts:', props.postState.posts)
+  
+  
+
+
   return (
     <div>
       {props.postState.posts.map((post) => (
         <Post key={post.id} post={post} />
-      ))}
+        ))}
+        
+        <MapWapper trailId={trailId} />
     </div>
   )
 }
