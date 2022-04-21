b8c72c6432f4d9cc9387eaf8cc869423124054c5
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-20 19:21:37 -0600

    bottom nav bear

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,9 +6,13 @@ const Footer = () => {
 return (
  <div className="footer-design">
    <div className="tree-container">
-    <img className ="treeOne" src="https://i.imgur.com/t85I3wt.png" alt="treeOne"/>
-    <img className ="treeTwo" src="https://i.imgur.com/2qmPB9z.png" alt="treeTwo"/>
-    <img className ="treeThree" src="https://i.imgur.com/9AAkXsQ.png" alt="treeThree"/>
+    <img className ="treeTwo" src="https://i.imgur.com/RBfjlnm.png" alt="treeTwo"/>
+    <img className ="tent" src="https://i.imgur.com/0b0Qy89.png" alt="tent"/>
+    <img className ="hiker" src="https://i.imgur.com/bLtGulS.png" alt="hiker"/>
+    <img className ="treeOne" src="https://i.imgur.com/l2m1kKb.png" alt="treeOne"/>
+    <img className ="treeTwo" src="https://i.imgur.com/RBfjlnm.png" alt="treeTwo"/>
+    <img className ="treeThree" src="https://i.imgur.com/jlwfewH.png" alt="treeThree"/>
+    <img className ="bear" src="https://i.imgur.com/4fWKGuS.png" alt="bear"/>
    </div>
   <hr className="ground-hr" />
 </div> 
