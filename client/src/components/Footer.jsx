9816f026467cb08dcb167f53d5440c22746883f9
Jenna Leopold <jleopold424@gmail.com>
2022-04-20 20:58:43 -0400

    anchor dancing trees

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,10 +4,13 @@ const Footer = () => {
 
 
 return (
- <div className='footer-design'>
-  <img className ="treeOne" src="https://i.imgur.com/t85I3wt.png" alt="treeOne"/>
-  <img className ="treeTwo" src="https://i.imgur.com/2qmPB9z.png" alt="treeTwo"/>
-  <img className ="treeThree" src="https://i.imgur.com/9AAkXsQ.png" alt="treeThree"/>
+ <div className="footer-design">
+   <div className="tree-container">
+    <img className ="treeOne" src="https://i.imgur.com/t85I3wt.png" alt="treeOne"/>
+    <img className ="treeTwo" src="https://i.imgur.com/2qmPB9z.png" alt="treeTwo"/>
+    <img className ="treeThree" src="https://i.imgur.com/9AAkXsQ.png" alt="treeThree"/>
+   </div>
+  <hr className="ground-hr" />
 </div> 
 )
 }
